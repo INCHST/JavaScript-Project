6d8f905739131b24993b92c8416e587d550838e3
unknown <inyastephanas@gmail.com>
2025-04-20 09:37:11 +0100

    first commit

--- arrayofstringsinalph.js
+++ arrayofstringsinalph.js
@@ -1,9 +1,20 @@
-function arrayAlphabetically(school) {
-    return school.sort();
-  }
+// function arrayAlphabetically(school) {
+//     return school.sort();
+//   }
   
+//   let schools = ["Lasustech", "Unilag", "Kwasu", "Aocoed", "Noun"];
+//   let sortedSchools = arrayAlphabetically(schools);
+//   console.log(sortedSchools);
+
+
   let schools = ["Lasustech", "Unilag", "Kwasu", "Aocoed", "Noun"];
-  let sortedSchools = arrayAlphabetically(schools);
-  console.log(sortedSchools);
+  console.log(schools.sort())
+
+
+
+
+
+
+
   
     
