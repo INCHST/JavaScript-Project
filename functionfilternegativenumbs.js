3fdf80a6fa34edf905a60f4c4ad7c17d2e8b1ebd
unknown <inyastephanas@gmail.com>
2025-04-20 09:16:29 +0100

    first commit

--- functionfilternegativenumbs.js
+++ functionfilternegativenumbs.js
@@ -1,10 +1,6 @@
-function filterNegativeNumbers(array) {
-    return array.filter(nums => nums >= 0);
-  }
-
-  let numbers = [-1, 2, -3, 4, -5, 6];
-  let filteredNumbers = filterNegativeNumbers(numbers);
-  console.log(filteredNumbers);
+let numbers = [-1, 2, -3, 4, -5, 6];
+  
+console.log(numbers.filter(num => num >= 0));
   
   
   
