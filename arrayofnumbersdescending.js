f9ab2ebe5e3adbecb08400348ff058781de7913d
unknown <inyastephanas@gmail.com>
2025-04-20 09:03:33 +0100

    first commit

--- arrayofnumbersdescending.js
+++ arrayofnumbersdescending.js
@@ -1,9 +1,5 @@
-function sortDescending(num) {
-    return num.sort((a, b) => b - a);
-  }
+let numbers = [4, 2, 7, 1, 3];
 
-  let numbers = [4, 2, 7, 1, 3];
-  let sortedNumbers = sortDescending(numbers);
-  console.log(sortedNumbers);
+console.log(numbers.sort((a,b) => b - a));
   
   
